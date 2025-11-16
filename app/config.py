848752b2214cf90597b66fc649ec3249dc774573
Kira <90696175+KiraPixel@users.noreply.github.com>
2025-11-16 17:02:11 +0300

    change build and push

--- app/config.py
+++ app/config.py
@@ -1,12 +1,12 @@
 import os
 from passlib.context import CryptContext
 
-SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
+SECRET_KEY = os.getenv('CP_SECRET_KEY', 'default_secret_key')
 
-SQLALCHEMY_DATABASE_URL = os.getenv('SQLALCHEMY_DATABASE_URL', 'sqlite:///default.db')
+SQLALCHEMY_DATABASE_URL = os.getenv('CP_SQLALCHEMY_DATABASE_URL', 'sqlite:///default.db')
 
-HOST = os.getenv('HOST', '0.0.0.0')
-PORT = os.getenv('PORT', '5000')
+HOST = os.getenv('CP_HOST', '0.0.0.0')
+PORT = os.getenv('CP_PORT', '5000')
 
 ALGORITHM = "HS256"
 PWD_CONTEXT = CryptContext(schemes=["bcrypt"], deprecated="auto")
